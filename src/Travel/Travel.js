40b06315cbfbe7baa4d53f8a3b2c807f98d8a063
“Erin <erinkelley27@gmail.com>
2020-01-17 15:33:20 -0500

    added charles hotel room block link

--- src/Travel/Travel.js
+++ src/Travel/Travel.js
@@ -18,9 +18,9 @@ const Travel = () => {
                         <p>Harvard Square One Bennett Street</p>
                         <p>Cambridge, MA 02138</p>
                     </a>
-                    <a className="deadline" href="" target="_blank">
+                    <a className="deadline" href="https://be.synxis.com/?adult=1&arrive=2020-03-20&chain=10237&child=0&configcode=BOSCH&currency=USD&depart=2020-03-22&group=KLWEDDING20&hotel=27199&level=hotel&locale=en-US&rooms=1&themecode=BOSCH&trifecta=1" target="_blank">
                         <p className="deadline">Link for Kelley-Lawrence Wedding Block</p>
-                        <p>Deadline: Februrary 20, 2020</p>
+                        <p>Deadline: Februrary 21, 2020</p>
                     </a>
                 </div>
             </div>
