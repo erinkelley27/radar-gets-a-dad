0450c18956ffa817031c2f608f3ffc5e99807c48
“Erin <erinkelley27@gmail.com>
2020-02-11 11:57:07 -0500

    updated ceremony info and photo

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Events.css';
 
+import centanni from "../assets/centanni.png"
 import esplanade from "../assets/esplanade-watercolor.png"
 import pammys from "../assets/pammys.jpg"
 
@@ -8,18 +9,19 @@ const Events = () => {
     return (
         <div className="Events">
             <div className="ceremony-location">
-                <img className="ceremony-location-image" src={esplanade} alt="esplanade" />
+                <img className="ceremony-location-image" src={centanni} alt="esplanade" />
                 <div className="ceremony-event-info">
-                    <a className="location-name" target="_blank" href="https://goo.gl/maps/yz7a3DK36qs7PzwBA">
-                        <h2>Ceremony</h2>
-                        <h3>To Be Announced</h3>
+                    <a className="location-name" target="_blank" href="https://goo.gl/maps/oCWrfP1uCTMUvkso6">
+                        <div className="location-bundle">
+                            <h2>Ceremony</h2>
+                            <h3>Centanni Park</h3>
+                            <p>Cambridge, MA</p>
+                        </div>
                     </a>
-                    <h4>Saturday, March 21, 2020</h4>
-                    <h4>5:30pm</h4>
-                    {/* <a className="location-address" target="_blank" href="https://goo.gl/maps/JCJTrJZm8Dq89itM9">
-                        <p>928 Massachussetts Avenue</p>
-                        <p>Cambridge, MA 02139</p>
-                    </a> */}
+                    <div className="date-time-bundle">
+                        <h4>Saturday, March 21, 2020</h4>
+                        <h4>5:30pm</h4>
+                    </div>
                 </div>
             </div>
             <div className="reception-location">
@@ -29,12 +31,14 @@ const Events = () => {
                         <h2>Reception</h2>
                         <h3>Pammy's</h3>
                     </a>
-                    <h4>Saturday, March 21, 2020</h4>
-                    <h4>6:00pm</h4>
                     <a className="location-address" target="_blank" href="https://goo.gl/maps/JCJTrJZm8Dq89itM9">
                         <p>928 Massachussetts Avenue</p>
                         <p>Cambridge, MA 02139</p>
                     </a>
+                    <div className="date-time-bundle">
+                        <h4>Saturday, March 21, 2020</h4>
+                        <h4>6:00pm</h4>
+                    </div>
                 </div>
             </div>
         </div>
